90498d9de62e3548eaee0c4e4083853c65117f0e
jarno018 <jarno.lannoo@hotmail.com>
2021-05-24 16:54:49 +0200

    added rotation detection and button actions

--- EncoderAndLeds/src/main.cpp
+++ EncoderAndLeds/src/main.cpp
@@ -3,6 +3,12 @@
 
 #define ADDRESS 0x20
 
+int pwmR = 0;
+int pwmG = 0;
+int pwmB = 0;
+
+byte prevEncoderValue = B00000000;
+
 const byte settings[6][2] = {
   //{address, setting}
   {0x00, 0xFF}, // Set portA as input for encoders
@@ -46,6 +52,129 @@ void writeRedLed(bool state) {
   Wire.endTransmission();
 }
 
+int fetchFromAddress(byte addr) {
+  byte result;
+  Wire.beginTransmission(ADDRESS);
+  Wire.write(addr);
+  Wire.endTransmission();
+  Wire.requestFrom(ADDRESS, 1);
+  while (Wire.available()) {
+    result = Wire.read();
+  }
+  return result;
+}
+
+/*
+Encoder for Red:
+Clockwise           xxxx xx00 -> xxxx xx01 -> xxxx xx11
+                    xxxx xx11 -> xxxx xx10 -> xxxx xx00
+Counter-clockwise   xxxx xx00 -> xxxx xx10 -> xxxx xx11
+                    xxxx xx11 -> xxxx xx01 -> xxxx xx00
+
+Encoder for Green:
+Clockwise           xxxx 00xx -> xxxx 01xx -> xxxx 11xx
+Counter-clockwise   xxxx 00xx -> xxxx 10xx -> xxxx 11xx
+
+Encoder for blue:
+Clockwise           xx00 xxxx -> xx01 xxxx -> xx11 xxxx
+Counter-clockwise   xx00 xxxx -> xx01 xxxx -> xx11 xxxx
+*/
+void handleEncoder() {
+  byte result = fetchFromAddress(0x10);
+
+  byte currentResultRed = result & 0x03;
+  byte currentResultGreen = (result & 0x0C) >> 2;
+  byte currentResultBlue = (result & 0x30) >> 4;
+
+  byte prevResultRed = prevEncoderValue & 0x03;
+  byte prevResultGreen = (prevEncoderValue & 0x0C) >> 2;
+  byte prevResultBlue = (prevEncoderValue & 0x30) >> 4;
+
+  // Detect change in red
+
+  // Clockwise
+  if(((prevResultRed == 0x00 && currentResultRed == 0x01) || (prevResultRed == 0x01 && currentResultRed == 0x03)) || ((prevResultRed == 0x03 && currentResultRed == 0x02) || (prevResultRed == 0x02 && currentResultRed == 0x00))) {
+    pwmR += 5;
+    if(pwmR > 255) pwmR = 255;
+  }
+
+  //Counter-Clockwise
+  if(((prevResultRed == 0x00 && currentResultRed == 0x02) || (prevResultRed == 0x02 && currentResultRed == 0x03)) || ((prevResultRed == 0x03 && currentResultRed == 0x01) || (prevResultRed == 0x01 && currentResultRed == 0x00))) {
+    pwmR -= 5;
+    if(pwmR < 0) pwmR = 0;
+  }
+
+
+  // Detect change in green
+  // Clockwise
+  if(((prevResultGreen == 0x00 && currentResultGreen == 0x01) || (prevResultGreen == 0x01 && currentResultGreen == 0x03)) || ((prevResultGreen == 0x03 && currentResultGreen == 0x02) || (prevResultGreen == 0x02 && currentResultGreen == 0x00))) {
+    pwmG += 5;
+    if(pwmG > 255) pwmG = 255;
+  }
+
+  //Counter-Clockwise
+  if(((prevResultGreen == 0x00 && currentResultGreen == 0x02) || (prevResultGreen == 0x02 && currentResultGreen == 0x03)) || ((prevResultGreen == 0x03 && currentResultGreen == 0x01) || (prevResultGreen == 0x01 && currentResultGreen == 0x00))) {
+    pwmG -= 5;
+    if(pwmG < 0) pwmG = 0;
+  }
+
+
+  // Detect change in blue
+  // Clockwise
+  if (((prevResultBlue == 0x00 && currentResultBlue == 0x01) || (prevResultBlue == 0x01 && currentResultBlue == 0x03)) || ((prevResultBlue == 0x03 && currentResultBlue == 0x02) || (prevResultBlue == 0x02 && currentResultBlue == 0x00))) {
+    pwmB += 5;
+    if (pwmB > 255) pwmB = 255;
+  }
+
+  //Counter-Clockwise
+  if(((prevResultBlue == 0x00 && currentResultBlue == 0x02) || (prevResultBlue == 0x02 && currentResultBlue == 0x03)) || ((prevResultBlue == 0x03 && currentResultBlue == 0x01) || (prevResultBlue == 0x01 && currentResultBlue == 0x00))) {
+    pwmB -= 5;
+    if(pwmB < 0) pwmB = 0;
+  }
+
+  prevEncoderValue = result;
+
+}
+
+void reset() {
+  // Reset the code and the colorvalues
+}
+
+void previousNumber() {
+  // Reset the colorvalues and get previous digit of code
+}
+
+void nextNumber() {
+  // Reset the colorvalues and get next digit of code
+}
+
+/*
+R-button (reset):
+1xxx xxxx -> not pressed
+0xxx xxxx -> pressed
+
+G-button (prev):
+x1xx xxxx -> not pressed
+x0xx xxxx -> pressed
+
+B-button (next):
+xx1x xxxx -> not pressed
+xx0x xxxx -> pressed
+*/
+void handleButton() {
+  byte result = ~fetchFromAddress(0x11); // Watch the flip
+
+  // Handle reset (left)
+  if(result & 0x80) reset();
+
+  // Handle previous (middle)
+  if(result & 0x40) previousNumber();
+
+  // Handle next (right)
+  if(result & 0x20) nextNumber();
+
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -57,26 +186,10 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   if(digitalRead(2)) {
-    byte resultPortA;
-    Wire.beginTransmission(ADDRESS);
-    Wire.write(0x10);
-    Wire.endTransmission();
-    Wire.requestFrom(ADDRESS, 1);
-    while (Wire.available()) {
-      resultPortA = Wire.read();
-    }
-    Serial.println(resultPortA, BIN);
+    handleEncoder();
   }
   if(digitalRead(3)) {
-    byte resultPortB;
-    Wire.beginTransmission(ADDRESS);
-    Wire.write(0x11);
-    Wire.endTransmission();
-    Wire.requestFrom(ADDRESS, 1);
-    while (Wire.available()) {
-      resultPortB = Wire.read();
-    }
-    Serial.println(resultPortB, BIN); // If only 7 bits get printed, first 0 is not printed, there is no zero-fill
+    handleButton();
   }
 
 }
